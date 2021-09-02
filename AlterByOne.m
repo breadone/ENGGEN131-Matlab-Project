1b8ae6b6b192d1bf985b3aa3721d03ac2ae2f4d1
pradyun <pradyun.setti@gmail.com>
2021-09-02 16:21:37 +1200

    restructured and added comments

--- AlterByOne.m
+++
@@ -1,7 +0,0 @@
-function newUInt = AlterByOne(oldUInt)
-    if oldUInt == 255
-        newUInt = 254;
-    elseif oldUInt < 255
-        newUInt = oldUInt + 1;
-    end
-end
---
+++ Part 1/AlterByOne.m
@@ -0,0 +1,8 @@
+function newUInt = AlterByOne(oldUInt)
+% returns a UINT value from one of two selections: if the value is 255,
+% then it returns 254, else it returns the input value plus one
+% input: a UInt8
+% output: another UInt8
+% Author: breadone
+    if oldUInt == 255; newUInt = 254; else; newUInt = oldUInt + 1;
+end
